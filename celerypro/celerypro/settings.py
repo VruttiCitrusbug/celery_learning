bc9a08b53e98f039b6a0cb55d7d14d5b68d0f078
unknown <vrutti.citrubsug@gmail.com>
2022-11-18 11:54:12 +0530

    updated 18-11

--- celerypro/celerypro/settings.py
+++ celerypro/celerypro/settings.py
@@ -128,10 +128,11 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = "django-db"
+CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 import environ
 env = environ.Env()
